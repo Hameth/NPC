011fdfe90bb040f43465bf2be2d563ea2ea24222
Hameth Abdhala Assaf Carrascal <hamethabdhala@icloud.com>
2020-05-03 17:21:43 -0500

    Minor Fixes

---
+++ Assets/Scripts/TransitionManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TransitionManager: MonoBehaviour
+{
+    public void ChangeToChapter1()
+    {
+        SceneManager.LoadScene(3);
+    }
+
+
+    public void ChangeToTheory()
+    {
+        SceneManager.LoadScene(4);
+    }
+
+    public void ChangeToHome()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void ChangeToExercise()
+    {
+        SceneManager.LoadScene(5);
+    }
+
+    public void ChangeToGame()
+    {
+        SceneManager.LoadScene(8);
+    }
+}
