7e453b14fe43be0813bfcbe8f2400115c79c1eca
KevinPicon01 <kevin.picon.2016@upb.edu.co>
2020-05-03 05:43:45 -0500

    Trivia
    La trivia funciona, da puntaje final despues de responder las preguntas

--- Assets/Scripts/Quiz.cs
+++ Assets/Scripts/Quiz.cs
@@ -1,20 +1,86 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+
 public class Quiz : MonoBehaviour
 {
+    
+    [SerializeField] private QuizUI _quizUi;
+    [SerializeField] private List<Question> _questions;
+    [SerializeField] private int size;
+    [SerializeField] private TextMeshProUGUI _score;
+    [SerializeField] private GameObject _menu;
+    private int cnt = 0;
+    private int score = 0;
+    private Question selectedQuestion;
+    
     // Start is called before the first frame update
     void Start()
     {
-        SceneManager.LoadScene("Prueba");
+        SelectQuestion();
+    }    
+ 
+    // Update is called once per frame
+    void SelectQuestion()
+    {
+        
+        selectedQuestion = _questions[cnt];
+        _quizUi.SetQuestion(selectedQuestion);
+        
+
     }
 
-    // Update is called once per frame
-    void Update()
+    private void GetResult(int pts)
     {
-        Debug.Log("entro");
-        SceneManager.LoadScene("Prueba");
+        _menu.transform.position = new Vector3(0,0,222222);
+        _score.text = "Puntaje\n \n"+pts;
+       
     }
+
+    private void Update()
+    {
+        if (cnt == _questions.Count)
+        {
+            GetResult(score);
+        }
+    }
+
+    public bool Answer(string answered)
+
+    {
+        bool correctAns = false;
+        if (answered == selectedQuestion.correctAns)
+        {
+            //yes
+            correctAns = true;
+            score++;
+            cnt++;
+        }
+        else
+        {
+            //no
+            cnt++;
+        }
+         Invoke("SelectQuestion",0.4f);
+         return correctAns;
+    }
+    
+}
+[System.Serializable]
+public class Question
+{
+    public string question;
+    public string correctAns;
+    public List<string> options;
+    public QuestionType questionType;
+
+}
+[System.Serializable]
+public enum QuestionType
+{
+    TEXT
 }
---
+++ Assets/Scripts/QuizUI.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using TMPro.Examples;
+using Unity.Mathematics;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class QuizUI : MonoBehaviour
+{
+    [SerializeField] private Quiz _quiz;
+    [SerializeField] private TextMeshProUGUI questionTxt;
+
+    [SerializeField] private List<Button> options;
+
+    [SerializeField] private Color correctCol, wrongCol, normalCol;
+
+    private Question question;
+
+    private bool answered;
+    
+    
+    // Start is called before the first frame update
+    void Awake()
+    {
+        for (int i = 0; i < options.Count; i++)
+        {
+            Button localBtn = options[i];
+            localBtn.onClick.AddListener(() => Onclick(localBtn));
+        }
+    }
+
+    public void SetQuestion(Question question)
+    {
+        this.question = question;
+        questionTxt.text = question.question;
+
+        List<string> answerList = question.options;
+        for (int i = 0; i < options.Count; i++)
+        {
+            options[i].GetComponentInChildren<TextMeshProUGUI>().text = answerList[i];
+            
+            
+            
+            options[i].name = answerList[i];
+            options[i].image.color = normalCol;
+            
+        }
+
+        answered = false;
+    }
+
+    // Update is called once per frame
+    private void Onclick(Button btn)
+    {
+        if (!answered)
+        {
+            answered = true;
+            bool val = _quiz.Answer(btn.name);
+
+
+            if (val)
+            {
+                btn.image.color = correctCol;
+            }
+            else
+            {
+                btn.image.color = wrongCol;
+            }
+        }
+    }
+}
