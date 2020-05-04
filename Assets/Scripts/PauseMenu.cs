7b9a3d17448223b240181bb835b5b35d7fd720e0
Hameth Abdhala Assaf Carrascal <hamethabdhala@icloud.com>
2020-05-03 23:14:19 -0500

    Pause Menu

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject pauseMenuUi;
+    public GameObject menuButton;
+    
+    public void Resume()
+    {
+        menuButton.SetActive(true);
+        pauseMenuUi.SetActive(false);
+        Time.timeScale = 1;
+    }
+
+    public void Pause()
+    {
+        menuButton.SetActive(false);
+        pauseMenuUi.SetActive(true);
+        Time.timeScale = 0f;
+    }
+
+    public void Reload()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        Time.timeScale = 1;
+    }
+
+    public void Dead()
+    {
+        menuButton.SetActive(false);
+        pauseMenuUi.SetActive(true);
+        Time.timeScale = 0f;
+    }
+}
