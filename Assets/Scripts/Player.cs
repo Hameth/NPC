557dadb133f169a1ec1aec7151a120d0736ed2d2
Hameth Abdhala Assaf Carrascal <hamethabdhala@icloud.com>
2020-05-05 16:27:47 -0500

    Gyro controlls

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,25 +6,27 @@ using UnityEngine.SceneManagement;
 public class Player : MonoBehaviour
 {
     public float movementSpeed = 10f;
+    
     private float _movement;
+    private float _dirX;
     private Rigidbody2D _rb;
-    private PauseMenu controlPausa;
+    
+    private PauseMenu _controlPausa;
+    
     private void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
-        controlPausa = FindObjectOfType<PauseMenu>();
     }
 
     private void Update()
     {
-        _movement = Input.GetAxis("Horizontal") * movementSpeed;
+        _dirX = Input.acceleration.x * _movement * movementSpeed;
+        transform.position = new Vector2(Mathf.Clamp(transform.position.x, -7.5f, 7.5f), transform.position.y);
     }
 
     private void FixedUpdate()
     {
-        Vector2 velocity = _rb.velocity;
-        velocity.x = _movement;
-        _rb.velocity = velocity;
+        _rb.velocity = new Vector2(_dirX, 0f);
     }
     private void OnTriggerEnter2D(Collider2D other)
     {
@@ -34,8 +36,7 @@ public class Player : MonoBehaviour
         }
         if (!other.gameObject.CompareTag("Tip")) return;
         other.gameObject.SetActive(false);
-       
-        controlPausa.PauseTip(); 
+        _controlPausa.PauseTip(); 
         
     }
 }
